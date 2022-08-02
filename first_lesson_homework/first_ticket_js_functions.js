011468925633fb29c52012a3b035e9e4ffd04f21
kuptsovsasha <kuptsovsasha@gmail.com>
2022-08-02 22:28:46 +0300

    add task for first lesson

---
+++ first_lesson_homework/first_ticket_js_functions.js
@@ -0,0 +1,39 @@
+//Area calculator
+function calculateAreaOfCircle(){
+    const pNumber = 3.14159265
+
+    let inputValue = prompt("Add circle radius");
+    if (inputValue <= 0 || inputValue.includes(','))
+        return alert("You should add valid number!")
+
+    let calculatedArea = (inputValue ** 2) * pNumber;
+
+    return alert(calculatedArea);
+}
+
+// Retire Calculator
+
+function calculateTimeUntilRetire(){
+    const retireAge = 65
+    const currentYear = new Date().getFullYear();
+
+    let inputValue = prompt("Write your current age.");
+    if (inputValue <= 0)
+        return alert("You should add valid age!")
+    if (inputValue >= 65)
+        return alert("You already retire!")
+
+    let yearsToRetirement = retireAge - inputValue;
+    let retireYear = currentYear + yearsToRetirement;
+
+    return alert(`You retire through ${yearsToRetirement} years in ${retireYear}`);
+}
+
+// Reverse number
+
+function getReversedNumber(){
+
+    let inputValue = String(prompt("Write your number."));
+
+    return alert(inputValue.split('').reverse().join(''));
+}
