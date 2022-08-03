625506248b76974ccd54048bd2239bdf7f94f5f6
kuptsovsasha <kuptsovsasha@gmail.com>
2022-08-03 21:12:30 +0300

    change if checkers

--- first_lesson_homework/first_ticket_js_functions.js
+++ first_lesson_homework/first_ticket_js_functions.js
@@ -3,9 +3,9 @@ function calculateAreaOfCircle(){
     const pNumber = 3.14159265
 
     let inputValue = prompt("Add circle radius");
-    if (inputValue <= 0 || inputValue.includes(','))
-        return alert("You should add valid number!")
-
+    if (inputValue <= 0 || inputValue.includes(',')) {
+        return alert("You should add valid number!");
+    }
     let calculatedArea = (inputValue ** 2) * pNumber;
 
     return alert(calculatedArea);
@@ -18,22 +18,26 @@ function calculateTimeUntilRetire(){
     const currentYear = new Date().getFullYear();
 
     let inputValue = prompt("Write your current age.");
-    if (inputValue <= 0)
-        return alert("You should add valid age!")
-    if (inputValue >= 65)
-        return alert("You already retire!")
-
+    if (inputValue <= 0) {
+        return alert("You should add valid age!");
+    }
+    if (inputValue >= 65) {
+        return alert("You already retire!");
+    }
     let yearsToRetirement = retireAge - inputValue;
     let retireYear = currentYear + yearsToRetirement;
 
-    return alert(`You retire through ${yearsToRetirement} years in ${retireYear}`);
+    return alert(`You will retire after ${yearsToRetirement} years in ${retireYear}`);
 }
 
 // Reverse number
 
 function getReversedNumber(){
 
-    let inputValue = String(prompt("Write your number."));
-
-    return alert(inputValue.split('').reverse().join(''));
+    let inputValue = String(prompt("Write your number. The number should be three-digit"));
+    if (inputValue.length !== 3){
+        return alert("The number should be three-digit");
+    } else {
+        return alert(inputValue.split('').reverse().join(''));
+    }
 }
